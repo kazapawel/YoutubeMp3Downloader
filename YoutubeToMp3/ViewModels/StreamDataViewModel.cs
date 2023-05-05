aeca52bd5661ff2e0429fcd056575a11aa0c8d9b
kazapawel <kaza.pawel@gmail.com>
2023-05-05 19:30:54 +0200

    fixed thumbnail bug

--- YoutubeToMp3/ViewModels/StreamDataViewModel.cs
+++ YoutubeToMp3/ViewModels/StreamDataViewModel.cs
@@ -14,7 +14,7 @@
         public string Duration => _streamData?.Duration.ToString();
         public string Author => _streamData?.Author;
         public string UploadDate => _streamData?.UploadDate.Value.Date.ToShortDateString();
-        public string Thumbnail => _streamData != null ? _streamData?.Thumbnail : "pack://application:,,,/Images/youtube_modern_100.png";
+        public string Thumbnail => _streamData?.Thumbnail;
         public string VideoQuality => _streamData != null ? _streamData.VideoQuality : string.Empty;
         public string AudioBitrate => _streamData != null ? _streamData.AudioBitrate : string.Empty;
         public StreamData Model => _streamData;
